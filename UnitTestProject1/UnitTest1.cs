9e1026d3567cc1a8ce4970b33d553fb00ba7d080
Maria Grivorieva <masha_21.02.99@mail.ru>
2021-09-08 13:08:13 +0300

    Добавьте файлы проекта.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BasicUnitTest.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Sum_1and2_3_return()
+        {
+            // arrange
+            int x = 1;
+            int y = 2;
+            int expected = 3;
+
+            // act
+            TestClass1 c = new TestClass1();
+            int actual = c.Sum(x, y);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
